2d30c2b5e4d522d7eb9bf5aa265acbf1983ec5a7
Kimon Meier <kimon.meier@gmail.com>
2024-06-14 16:55:59 +0200

    Added Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Sound } from "svelte-sound";
+	import soundFile from "$lib/buzzer.mp3";
+	import { onMount } from "svelte";
+
+	const help = new Sound(soundFile);
+
+	onMount(() => {
+		help.play();
+	});
+</script>
+
+<slot></slot>
